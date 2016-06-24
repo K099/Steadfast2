b66bc00b3e63c9908ce97da5cc76c387e901706d
K099 <akephisitch@gmail.com>
2016-06-24 23:28:42 +0700

    Updat

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -3285,15 +3285,9 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer{
 		}
 		
 		if($this->getFood()-$amount <= 6 && !($this->getFood() <= 6)) {
-//			$this->setDataProperty(self::DATA_FLAG_SPRINTING, self::DATA_TYPE_BYTE, false);
-			$this->removeEffect(Effect::SLOWNESS);
+			$this->setDataProperty(self::DATA_FLAG_SPRINTING, self::DATA_TYPE_BYTE, false);
 		} elseif($this->getFood()-$amount < 6 && !($this->getFood() > 6)) {
-//			$this->setDataProperty(self::DATA_FLAG_SPRINTING, self::DATA_TYPE_BYTE, true);
-			$effect = Effect::getEffect(Effect::SLOWNESS);
-			$effect->setDuration(0x7fffffff);
-			$effect->setAmplifier(2);
-			$effect->setVisible(false);
-			$this->addEffect($effect);
+			$this->setDataProperty(self::DATA_FLAG_SPRINTING, self::DATA_TYPE_BYTE, true);
 		}
 		if($this->hunger - $amount < 0) return;
 		$this->setFood($this->getFood() - $amount);
